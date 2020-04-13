915b40703ac960a8ff4272ee5a6bec8291e84951
Steven Oldfield <steveold1611@gmail.com>
2020-04-13 10:53:03 -0600

    moving along - next stop winning

--- tictac/tic_tac_toe.py
+++ tictac/tic_tac_toe.py
@@ -11,7 +11,7 @@ thoughts on steps:
 --create unit tests for each section as needed
 """
 
-# Starting section
+# Starting section   V1.0
 print("Welcome to the greatest tic tac toe game ever created!!!!\n")
 print(""""The rules of this game are:
 1: Players decide who will be 'X' and 'O'.
@@ -20,7 +20,7 @@ print(""""The rules of this game are:
 
 location = {'loc1': '-', 'loc2': '-', 'loc3': '-', 'loc4': '-',
             'loc5': '-', 'loc6': '-', 'loc7': '-', 'loc8': '-', 'loc9': '-'}
-start_player = 0
+start_player = True
 first_player = 'x'
 second_player = 'x'
 
@@ -43,6 +43,15 @@ def displayGameBoard(location):
     print('   |   |   ')
     print('')
 
+def set_mark_board(loc, player):
+    pass
+    if start_player == True:
+        location['loc'+loc] = "X"
+        return
+    else:
+        location['loc'+loc] = "O"
+        return
+
 def game_start_launch():
     global start_player, first_player, second_player
     playgame = input("Would you like to play a game? (y/n):  ")
@@ -51,33 +60,59 @@ def game_start_launch():
         first_player = input("Name of first player:(this will be the 'X' person)  ")
         second_player = input("Name of second player:(this will be the 'O' person) ")
         if random() >= .5:
-            start_player = 1
-        if start_player == 0:
+            start_player = False
+        if start_player == True:
             print(f'\n{first_player} will start the game and go first')
         else:
             print(f'\n{second_player} will start the game and go first')    
-        game_turn(start_player)
+        game_turn()
     elif playgame.lower() == 'n':
         print("Goodbye")
         exit() 
     else:
         print('Please select either y or n:')
         game_start_launch()      
-   
 
-def game_turn(start_player):
+def validate_input(loc):
+    try:
+        val = int(loc)
+        return val
+    except ValueError:
+        print("You did not chose a number between 1 and 9: Please reselect")
+        return 
+       
+
+def game_turn():
     #pass
+    global start_player
+    current_player = start_player
     loc = input('Please select 1-9 to place your mark: ')
+    validate_input(loc)
+    set_mark_board(loc, current_player)
+    current_player = not current_player
+    start_player = current_player
+    print(start_player)
+    displayGameBoard(location)
+    #if start_player == 0:
+        #pass
 
+def game_win_check(location):
+    pass
+    '''
+1,2,3
+4,5,6
+7,8,9
+1,5,9
+3,5,7
+
+'''
 
 
 game_start_launch()
 
-
-
 ### location['loc1'] = "X"
 #print(location)
-displayGameBoard(location)
+#displayGameBoard(location)
 
 
 # test_display()
