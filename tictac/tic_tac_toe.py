c3c4c3bb32d46405b29b25e5eaa1bf1d93536b28
Steven Oldfield <steveold1611@gmail.com>
2020-04-22 22:20:41 -0600

    Version done - works now

--- tictac/tic_tac_toe.py
+++ tictac/tic_tac_toe.py
@@ -1,42 +1,23 @@
-""" req:
-- 2 players able to play on same computer
-- board will be reprinted after each move
-- accept input (move) and display symbol on board.
-
-thoughts on steps: 
--- create game framework (play game, replay, quit game) (if fancy ask for player names)
---create board display section
--- create 'symbols' section (part of the display section)
--- create inputs section (part of the framework section)
---create unit tests for each section as needed
-"""
-
-# Starting section   V1.0
-'''
+# Starting section   V1.10
 print("Welcome to the greatest tic tac toe game ever created!!!!\n")
 print(""""The rules of this game are:
 1: Players decide who will be 'X' and 'O'.
 2: Player that will start will be randomly decided.
-3: Player will determine where to place the game piece by selecting a number, the numbers will match the layout of the keyboard number pad.\n""")
-'''
-#location = {'loc1': '-', 'loc2': 'X', 'loc3': 'X', 'loc4': '-',
-# 'loc5': 'X', 'loc6': 'X', 'loc7': 'X', 'loc8': '-', 'loc9': '-'}
-
-#location = {'loc1': '-', 'loc2': '-', 'loc3': '-', 'loc4': '-',
-#            'loc5': '-', 'loc6': '-', 'loc7': 'O', 'loc8': 'O', 'loc9': 'O'}
+3: Player will determine where to place the game piece by selecting a number between 1-9, 
+   the gameboard locations will match the layout of the keyboard number pad.
+4: Select q to quit the game.\n""")
 
 location = {'loc1': '-', 'loc2': '-', 'loc3': '-', 'loc4': '-',
             'loc5': '-', 'loc6': '-', 'loc7': '-', 'loc8': '-', 'loc9': '-'}            
 
-win_combo = [('loc1','loc2','loc3'),('loc4','loc5','loc6'),('loc7','loc8','loc9'),('loc2','loc5','loc8'),('loc1','loc5','loc9'),('loc3','loc5','loc7')]
+win_combo = [('loc1','loc2','loc3'),('loc1','loc4','loc7'),('loc3','loc6','loc9'),('loc4','loc5','loc6'),('loc7','loc8','loc9'),('loc2','loc5','loc8'),('loc1','loc5','loc9'),('loc3','loc5','loc7')]
 
 start_player = True
 first_player = 'x'
 second_player = 'x'
 
 def displayGameBoard(location):
-    # pass
-    # clear display????
+    clear_display()
     print('   |   |   ')
     print(' ' + location['loc7'] + ' | ' +
           location['loc8'] + ' | ' + location['loc9'])
@@ -53,21 +34,28 @@ def displayGameBoard(location):
     print('   |   |   ')
     print('')
 
+def validate_square_not_selected_already(loc):
+    if location['loc'+loc] != '-':
+        print('Square is already selected, chose another square:')
+        game_turn()
+    else:
+        return
+
 def set_mark_board(loc, player):
     if start_player == True:
-        location['loc'+loc] = "X"
+        location['loc'+loc] = 'X'
         return
     else:
-        location['loc'+loc] = "O"
+        location['loc'+loc] = 'O'
         return
 
 def game_start_launch():
     global start_player, first_player, second_player
-    playgame = input("Would you like to play a game? (y/n):  ")
+    playgame = input('Would you like to play a game? (y/n):  ')
     if playgame.lower() == 'y':
-        from random import random
         first_player = input("Name of first player:(this will be the 'X' person)  ")
         second_player = input("Name of second player:(this will be the 'O' person) ")
+        from random import random
         if random() >= .5:
             start_player = False
         if start_player == True:
@@ -76,7 +64,7 @@ def game_start_launch():
             print(f'\n{second_player} will start the game and go first')    
         game_turn()
     elif playgame.lower() == 'n':
-        print("Goodbye")
+        print('Goodbye')
         exit() 
     else:
         print('Please select either y or n:')
@@ -85,88 +73,100 @@ def game_start_launch():
 def validate_input(loc):
     try:
         val = int(loc)
-        return val
+        if val == 1 or val == 2 or val == 3 or val == 4 or val == 5 or val == 6 or val == 7 or val == 8 or val == 9:
+            return val
+        else:
+            print('You did not chose a number between 1 and 9: Please reselect')
+            game_turn()  
     except ValueError:
-        print("You did not chose a number between 1 and 9: Please reselect")
-        return 
+        if loc == 'q' or loc == 'Q':
+            print('Goodbye')
+            exit()
+        else:
+            print('You did not chose a number between 1 and 9: Please reselect')
+            game_turn()
+    return    
        
+def clear_display():
+    from os import system, name
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _= system('clear')
 
 def game_turn():
-    #pass
     continue_game = True
     global start_player
     if continue_game == True:
         current_player = start_player
+        player_name = ''
+        if current_player == True:
+            player_name = first_player
+        else:
+            player_name = second_player
+        print(f'{player_name} Turn:')    
         loc = input('Please select 1-9 to place your mark: ')
         validate_input(loc)
+        validate_square_not_selected_already(loc)
         set_mark_board(loc, current_player)
-        game_win_check(location)
-        if game_win_check == 1:
-            print('WINNER!!!!')
-            exit()
+        displayGameBoard(location)
+        result = game_win_check(location)
+        if result == 1:
+            contine_game = False
+            if input('Would you like to play again? ') == 'y':
+                game_reset()
+                game_start_launch
+            else:
+                print('Good-Bye')
+                exit()
         else:
             current_player = not current_player
         start_player = current_player
         displayGameBoard(location)
     game_turn()
 
-    #if start_player == 0:
-        #pass
-
 def game_win_check(location):
     chk_x = []
     chk_o = []
     count = 0
-    count2 = 0
+    win_combo_count = 0
     match_count = 0
-    win_sub_counter = 0
-    
+    win_sub_counter = 1
     for i in location:
         if location.get(i) == 'X':
             chk_x.append(i)
         elif location.get(i) == 'O':
             chk_o.append(i)
-    while count < 6:
-        #print(win_sub)
+    if len(chk_x) + len(chk_o) == 9:
+        print('Game is a Draw!')
+        return 1 
+    while count < 8:
         if start_player == True:
             player_icon = chk_x
         else:
             player_icon = chk_o    
-            for beep in player_icon:
-                win_sub_counter += 1
-                win_sub = list(win_combo[count2])
-                #print(beep)
-                for blah in win_sub:
-                    if win_sub_counter == 3:
-                        count2 += 1
-                        match_count = 0
-                    if blah in beep:
-           # print(win_sub)
-             #       print('xx')
-                        match_count += 1
-                        if match_count == 3:
-                            print('We have a WINNER')
-                            return 1
-            count +=1
-        return 0   
+        for selected_square in player_icon:
+            win_sub_counter += 1
+            winning_subset_combo = list(win_combo[win_combo_count])
+            for winner_check_item in winning_subset_combo:
+                if winner_check_item in selected_square:
+                    match_count += 1
+                if match_count == 3:
+                    print('We have a WINNER!!!!')
+                    return 1
+        count +=1
+        match_count = 0
+        win_combo_count += 1
+    return 0   
 
 
+def game_reset():
+    global location, start_player
+    start_player = True
+    location = {'loc1': '-', 'loc2': '-', 'loc3': '-', 'loc4': '-',
+            'loc5': '-', 'loc6': '-', 'loc7': '-', 'loc8': '-', 'loc9': '-'} 
+    match_count = 0
 
 game_start_launch()
 
-#displayGameBoard(location)
-
-
-# test_display()
-###
-"""
-def test_display():
-    location = {'loc1': 'X', 'loc2': 'X', 'loc3': 'X', 'loc4': 'X',
-                'loc5': 'X', 'loc6': 'X', 'loc7': 'X', 'loc8': 'X', 'loc9': 'X'}
-    displayGameBoard(location)
-
-    location = {'loc1': 'O', 'loc2': 'O', 'loc3': 'O', 'loc4': 'O',
-                'loc5': 'O', 'loc6': 'O', 'loc7': 'O', 'loc8': 'O', 'loc9': 'O'}
-    displayGameBoard(location)
-"""
 
